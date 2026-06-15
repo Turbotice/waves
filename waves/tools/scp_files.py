1ced30fd1e52d96424c0f50e2b86b04a1e0e8889
sperrard <stephane.perrard@espci.fr>
2026-06-15 22:37:27 +0200

    add arg parser to specify subfolder

--- waves/tools/scp_files.py
+++ waves/tools/scp_files.py
@@ -1,15 +1,33 @@
 import subprocess
 import os
 import glob
+import argparse
 
-ipadour = "172.28.176.38"
-iplaita = "172.28.159.188"
-folder = '/media/turbots/DATA1/Jet_Surface/Basilisk/Forced/'#outputs/'
+def gen_parser():    
+    parser = argparse.ArgumentParser(description="Compile .h5 files in one folder")
+    parser.add_argument('-ow', dest='overwrite', type=bool,default=True,help='overwrite previous .h5 file')
+    parser.add_argument('-f', dest='folder', type=str,default="",help='To target a specific folder')
 
-filelist = glob.glob('*/moments.h5')
-print(filelist)
+    #parser.add_argument('-step', dest='step', type=int,default=3,help='select Step to be performed')
+#    print(parser)   
+    args = parser.parse_args()
+    print(args)
+    return args
 
-if not os.path.exists('outputs'):
-    os.makedirs('outputs')
 
-subprocess.run(["scp",f"turbots@{ipadour}:{folder}outputs/*",'outputs/'])
+def main(folder):
+    ipadour = "172.28.176.38"
+    iplaita = "172.28.159.188"
+    folder = '/media/turbots/DATA1/Jet_Surface/Basilisk/'+folder+'/'#outputs/'
+
+    filelist = glob.glob('*/moments.h5')
+    print(filelist)
+
+    if not os.path.exists('outputs'):
+        os.makedirs('outputs')
+
+    subprocess.run(["scp",f"turbots@{ipadour}:{folder}outputs/*",'outputs/'])
+
+if __name__ == '__main__':
+    args = gen_parser()
+    main(args.folder)
